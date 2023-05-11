0eb68736b9ac3d1db0e9354a8fd187828b7c7d8a
eiflores1 <106098659+eiflores1@users.noreply.github.com>
2023-05-10 23:19:25 -0500

    Update Universidad.java

--- ejercicio_java/Universidad.java
+++ ejercicio_java/Universidad.java
@@ -1,1 +1,17 @@
-public class Universidad {}
+public class Jugador { 
+//Variables    
+    private String Nombre;  
+// Constructores
+    public Jugador (){
+    } 
+// Metodos
+    public void setPersona (String Nombre){
+        this.Nombre = Nombre;
+    }
+    public String getNombre (){
+        return this.Nombre;
+    }
+    public static void main(String[]arg){
+        System.out.println("Nombre jugador");
+    }
+}
